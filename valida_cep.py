fbac821c8fd0c290f14be42f33d6344121413bfa
Gabriel Marcos MAgalhães <gabrielmarcosmag@gmail.com>
2020-08-05 17:42:57 -0300

    Melhoria na analise da expressão regular de formato do CEP

--- valida_cep.py
+++ valida_cep.py
@@ -11,8 +11,7 @@ def val_cep(cep):
 
     # Validação do formato do CEP: RegEx utilizada para determinar que o CEP não deve
     # começar em com 0 e que deve conter exatamente 6 caracateres numéricos
-    res = re.match(r'^[1-9]\d{5}$',cep)
-    if(res==None): return "Invalido"
+    if(re.match(r'^[1-9]\d{5}$',cep) == None): return "Invalido"
 
     #Verifica se existem digitos alternados repetitivos em par
     for i in range(4):
